3e21d539cfc9d7f956509d551c03e6f317a59eba
HEMANT GUPTA <hemantgupta8800@gmail.com>
2025-12-09 19:49:15 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main className="flex   flex-col items-center justify-between p-24 min-h-screen bg-gray-100">
+        <div className="h-150  w-2/4  rounded-sm bg-white px-4 box-border"></div>
+      </main>
+    </>
+  );
 }
